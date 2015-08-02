0e63bc93488bdbef1e8d3a6219c405d3fff63b70
Rohan Nagar <rohan.nagar@utexas.edu>
2015-08-01 21:44:11 -0500

    Add tests for UserResource

---
+++ application/src/test/java/com/sanction/thunder/resources/UserResourceTest.java
@@ -0,0 +1,74 @@
+package com.sanction.thunder.resources;
+
+import com.sanction.thunder.dao.StormUsersDao;
+import com.sanction.thunder.models.StormUser;
+import javax.ws.rs.core.Response;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+public class UserResourceTest {
+
+  private final StormUsersDao usersDao = mock(StormUsersDao.class);
+
+  private final UserResource resource = new UserResource(usersDao);
+
+  @Test
+  public void testPostNullUser() {
+    Response response = resource.postUser(null);
+
+    assertEquals(Response.Status.BAD_REQUEST, response.getStatusInfo());
+  }
+
+  @Test
+  public void testPostUserFailure() {
+    StormUser user = mock(StormUser.class);
+    when(usersDao.insert(user)).thenReturn(false);
+
+    Response response = resource.postUser(user);
+
+    assertEquals(Response.Status.SERVICE_UNAVAILABLE, response.getStatusInfo());
+  }
+
+  @Test
+  public void testPostUser() {
+    StormUser user = mock(StormUser.class);
+    when(usersDao.insert(user)).thenReturn(true);
+
+    Response response = resource.postUser(user);
+    boolean result = (boolean) response.getEntity();
+
+    assertEquals(Response.Status.CREATED, response.getStatusInfo());
+    assertEquals(true, result);
+  }
+
+  @Test
+  public void testGetUserWithNullUsername() {
+    Response response = resource.getUser(null);
+
+    assertEquals(Response.Status.BAD_REQUEST, response.getStatusInfo());
+  }
+
+  @Test
+  public void testGetUserFailure() {
+    when(usersDao.findByUsername("test")).thenReturn(null);
+
+    Response response = resource.getUser("test");
+
+    assertEquals(Response.Status.NOT_FOUND, response.getStatusInfo());
+  }
+
+  @Test
+  public void testGetUser() {
+    StormUser user = mock(StormUser.class);
+    when(usersDao.findByUsername("test")).thenReturn(user);
+
+    Response response = resource.getUser("test");
+    StormUser result = (StormUser) response.getEntity();
+
+    assertEquals(Response.Status.OK, response.getStatusInfo());
+    assertEquals(user, result);
+  }
+}
