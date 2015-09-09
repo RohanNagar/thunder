8febb0a3eb52872d33cd06095c9dff63e24866ae
Nick Eckert <n.eckert70@gmail.com>
2015-09-08 22:15:30 -0600

    Added authentication to thunder

--- application/src/main/java/com/sanction/thunder/ThunderApplication.java
+++ application/src/main/java/com/sanction/thunder/ThunderApplication.java
@@ -1,8 +1,12 @@
 package com.sanction.thunder;
 
+import com.sanction.thunder.authentication.Key;
+import com.sanction.thunder.authentication.ThunderAuthenticator;
 import com.sanction.thunder.dao.DaoModule;
 import com.sanction.thunder.dynamodb.DynamoDbModule;
 import io.dropwizard.Application;
+import io.dropwizard.auth.AuthFactory;
+import io.dropwizard.auth.basic.BasicAuthFactory;
 import io.dropwizard.setup.Bootstrap;
 import io.dropwizard.setup.Environment;
 
@@ -19,12 +23,14 @@ public class ThunderApplication extends Application<ThunderConfiguration> {
 
   @Override
   public void run(ThunderConfiguration config, Environment env) {
+
     ThunderComponent component = DaggerThunderComponent.builder()
         .daoModule(new DaoModule())
         .dynamoDbModule(new DynamoDbModule(config))
         .thunderModule(new ThunderModule())
         .build();
-
+    env.jersey().register(AuthFactory.binder(new BasicAuthFactory<>(new ThunderAuthenticator(
+        config.getApprovedKeys()), "AUTHENTICATION", Key.class)));
     env.jersey().register(component.getUserResource());
   }
 }
---
+++ application/src/main/java/com/sanction/thunder/authentication/ThunderAuthenticator.java
@@ -0,0 +1,37 @@
+package com.sanction.thunder.authentication;
+
+import com.google.common.base.Optional;
+
+import io.dropwizard.auth.AuthenticationException;
+import io.dropwizard.auth.Authenticator;
+
+import io.dropwizard.auth.basic.BasicCredentials;
+
+import java.util.List;
+
+public class ThunderAuthenticator implements Authenticator<BasicCredentials, Key> {
+
+  private final List<Key> allKeys;
+
+  public ThunderAuthenticator(List<Key> keys) {
+    this.allKeys = keys;
+  }
+
+  @Override
+  public Optional<Key> authenticate(BasicCredentials credentials) throws AuthenticationException {
+
+    // Construct a key from incoming credentials
+    Key key = new Key(credentials.getUsername(), credentials.getPassword());
+
+    if (key == null) {
+      return Optional.absent();
+    }
+
+    // Check if that key exists in the list of approved keys
+    if (!allKeys.contains(key)) {
+      return Optional.absent();
+    }
+
+    return Optional.of(key);
+  }
+}
