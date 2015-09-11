dd94d3672f8d693d21dab38f26731153705360d9
Rohan Nagar <rohan.nagar@utexas.edu>
2015-09-11 16:42:20 -0500

    Deleted Healthcheck for refactor

--- application/src/main/java/com/sanction/thunder/ThunderComponent.java
+++ application/src/main/java/com/sanction/thunder/ThunderComponent.java
@@ -1,7 +1,6 @@
 package com.sanction.thunder;
 
 import com.sanction.thunder.dao.DaoModule;
-import com.sanction.thunder.dynamodb.DynamoDbHealthCheck;
 import com.sanction.thunder.dynamodb.DynamoDbModule;
 import com.sanction.thunder.resources.UserResource;
 import dagger.Component;
