fecdeff4b9e59dd3f44a72ae66408791d73c2f1a
Rohan Nagar <rohan.nagar@utexas.edu>
2015-10-20 11:47:06 -0500

    Add PilotUser serialization tests

---
+++ api/src/test/java/com/sanction/thunder/models/PilotUserTest.java
@@ -0,0 +1,42 @@
+package com.sanction.thunder.models;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import io.dropwizard.jackson.Jackson;
+import io.dropwizard.testing.FixtureHelpers;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class PilotUserTest {
+  private static final ObjectMapper MAPPER = Jackson.newObjectMapper();
+
+  @Test
+  public void testToJson() throws Exception {
+    PilotUser pilotUser = new PilotUser(
+        "Test",
+        "12345",
+        "facebookAccessToken",
+        "twitterAccessToken",
+        "twitterAccessSecret");
+
+    String expected = MAPPER.writeValueAsString(
+        MAPPER.readValue(FixtureHelpers.fixture("fixtures/pilotUser.json"), PilotUser.class));
+
+    assertEquals(MAPPER.writeValueAsString(pilotUser), expected);
+  }
+
+  @Test
+  public void testFromJson() throws Exception {
+    PilotUser pilotUser = new PilotUser(
+        "Test",
+        "12345",
+        "facebookAccessToken",
+        "twitterAccessToken",
+        "twitterAccessSecret");
+
+    PilotUser fromJson = MAPPER.readValue(
+        FixtureHelpers.fixture("fixtures/pilotUser.json"), PilotUser.class);
+
+    assertEquals(fromJson, pilotUser);
+  }
+}
