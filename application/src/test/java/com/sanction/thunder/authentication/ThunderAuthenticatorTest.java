ea9be944d7379615beb8435db75666b331a1a247
Rohan Nagar <rohan.nagar@utexas.edu>
2016-04-06 13:56:06 -0500

    Add ThunderAuthenticator unit tests

---
+++ application/src/test/java/com/sanction/thunder/authentication/ThunderAuthenticatorTest.java
@@ -0,0 +1,53 @@
+package com.sanction.thunder.authentication;
+
+import com.google.common.base.Optional;
+import com.google.common.collect.Lists;
+
+import io.dropwizard.auth.AuthenticationException;
+import io.dropwizard.auth.basic.BasicCredentials;
+
+import java.util.List;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+public class ThunderAuthenticatorTest {
+  private final Key key = new Key("application", "secret");
+  private final List<Key> keys = Lists.newArrayList(key);
+  private final ThunderAuthenticator authenticator = new ThunderAuthenticator(keys);
+
+  @Test
+  public void testAuthenticateWithValidCredentials() {
+    BasicCredentials credentials = new BasicCredentials("application", "secret");
+
+    Optional<Key> result = Optional.absent();
+    try {
+      result =  authenticator.authenticate(credentials);
+    } catch (AuthenticationException e) {
+      // This shouldn't happen, so fail the test.
+      assertTrue(false);
+    }
+
+    assertTrue(result.isPresent());
+    assertEquals(key, result.get());
+  }
+
+  @Test
+  public void testAuthenticateWithInvalidCredentials() {
+    BasicCredentials credentials = new BasicCredentials("invalidApplication", "secret");
+
+    Optional<Key> result = Optional.absent();
+    try {
+      result =  authenticator.authenticate(credentials);
+    } catch (AuthenticationException e) {
+      // This shouldn't happen, so fail the test.
+      assertTrue(false);
+    }
+
+    assertFalse(result.isPresent());
+  }
+
+}
