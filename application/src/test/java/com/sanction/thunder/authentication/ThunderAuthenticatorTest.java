700b3827d4673d8282d3b4640a98f1db2a1f75da
Rohan Nagar <rohan.nagar@utexas.edu>
2016-04-17 20:16:35 -0500

    Use fail() method in tests

--- application/src/test/java/com/sanction/thunder/authentication/ThunderAuthenticatorTest.java
+++ application/src/test/java/com/sanction/thunder/authentication/ThunderAuthenticatorTest.java
@@ -13,6 +13,7 @@ import org.junit.Test;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.fail;
 
 public class ThunderAuthenticatorTest {
   private final Key key = new Key("application", "secret");
@@ -28,7 +29,7 @@ public class ThunderAuthenticatorTest {
       result =  authenticator.authenticate(credentials);
     } catch (AuthenticationException e) {
       // This shouldn't happen, so fail the test.
-      assertTrue(false);
+      fail();
     }
 
     assertTrue(result.isPresent());
@@ -44,7 +45,7 @@ public class ThunderAuthenticatorTest {
       result =  authenticator.authenticate(credentials);
     } catch (AuthenticationException e) {
       // This shouldn't happen, so fail the test.
-      assertTrue(false);
+      fail();
     }
 
     assertFalse(result.isPresent());
