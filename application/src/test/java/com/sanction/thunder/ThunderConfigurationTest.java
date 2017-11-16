718b0fcce67d0e8f4c86b9ec1252303470f082c0
Rohan Nagar <rohan.nagar@utexas.edu>
2017-11-15 18:22:05 -0800

    Add DynamoDB Configuration with endpoint and region

--- application/src/test/java/com/sanction/thunder/ThunderConfigurationTest.java
+++ application/src/test/java/com/sanction/thunder/ThunderConfigurationTest.java
@@ -3,28 +3,40 @@ package com.sanction.thunder;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.dataformat.yaml.YAMLFactory;
 
+import com.google.common.io.Resources;
 import com.sanction.thunder.authentication.Key;
 
+import io.dropwizard.configuration.YamlConfigurationFactory;
 import io.dropwizard.jackson.Jackson;
-import io.dropwizard.testing.FixtureHelpers;
+import io.dropwizard.jersey.validation.Validators;
 
+import java.io.File;
 import java.util.Arrays;
+import javax.validation.Validator;
+
 import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
 
 public class ThunderConfigurationTest {
-  private static final ObjectMapper MAPPER = Jackson.newObjectMapper(new YAMLFactory());
+  private final ObjectMapper mapper = Jackson.newObjectMapper(new YAMLFactory());
+  private final Validator validator = Validators.newValidator();
+  private final YamlConfigurationFactory<ThunderConfiguration> factory
+      = new YamlConfigurationFactory<>(ThunderConfiguration.class, validator, mapper, "dw");
 
   @Test
   public void testFromYaml() throws Exception {
-    ThunderConfiguration configuration = MAPPER.readValue(
-        FixtureHelpers.fixture("fixtures/config.yaml"), ThunderConfiguration.class);
+    ThunderConfiguration configuration = factory.build(
+        new File(Resources.getResource("fixtures/config.yaml").toURI()));
 
-    assertEquals("sample-table", configuration.getDynamoTableName());
+    assertEquals("test.dynamodb.com", configuration.getDynamoConfiguration().getEndpoint());
+    assertEquals("test-region-1", configuration.getDynamoConfiguration().getRegion());
+    assertEquals("test-table", configuration.getDynamoConfiguration().getTableName());
+
     assertEquals("test.email.com", configuration.getEmailConfiguration().getEndpoint());
-    assertEquals("test-region", configuration.getEmailConfiguration().getRegion());
+    assertEquals("test-region-2", configuration.getEmailConfiguration().getRegion());
     assertEquals("test@sanctionco.com", configuration.getEmailConfiguration().getFromAddress());
+
     assertEquals(
         Arrays.asList(new Key("test-app", "test-secret")),
         configuration.getApprovedKeys());
