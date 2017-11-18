a3048db2406006331489917de20be84a7e8649b5
Rohan Nagar <rohan.nagar@utexas.edu>
2017-11-17 23:01:00 -0800

    Run shell script

--- scripts/run-integration-tests.sh
+++ scripts/run-integration-tests.sh
@@ -4,7 +4,7 @@ echo "Starting Thunder and running Node.js integation tests..."
 
 java -jar application/target/application-*.jar server test-config.yaml &
 THUNDER_PID=$!
-sleep 2
+sleep 5
 node scripts/src/test-runner.js
 kill -9 $THUNDER_PID
 
