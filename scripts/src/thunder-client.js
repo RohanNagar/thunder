672c81e71eb68dd4c2fcfe23407bbc1f93775140
Rohan Nagar <rohan.nagar@utexas.edu>
2018-04-02 19:45:32 +0000

    Fix all ESLint problems

--- scripts/src/thunder-client.js
+++ scripts/src/thunder-client.js
@@ -9,7 +9,17 @@ const Method = {
   EMAIL:  { name: 'EMAIL', expected: 200 }
 };
 
+/**
+ * The ThunderClient class provides methods to talk to Thunder with.
+ */
 class ThunderClient {
+  /**
+   * Constructs a new ThunderClient.
+   * @constructor
+   *
+   * @param {string} endpoint - The base endpoint to connect to Thunder with.
+   * @param {object} auth - The basic authentication object.
+   */
   constructor(endpoint, auth) {
     this.baseRequest = request.defaults({
       baseUrl: endpoint,
@@ -17,7 +27,14 @@ class ThunderClient {
     });
   }
 
-  /* POST /users */
+  /**
+   * POST /users
+   * Creates a new Thunder user.
+   *
+   * @param {object} body - The request body to POST.
+   * @param {function} callback - The function to call when the method completes.
+   * @param {boolean} verbose - Whether to print verbose logs or not. Defaults to false.
+   */
   createUser(body, callback, verbose=false) {
     this.baseRequest.post({
       url:  '/users',
@@ -30,7 +47,15 @@ class ThunderClient {
     });
   }
 
-  /* GET /users */
+  /**
+   * GET /users
+   * Gets a Thunder user.
+   *
+   * @param {object} params - The query parameters to attach to the URL.
+   * @param {object} headers - The request headers to add to the request.
+   * @param {function} callback - The function to call when the method completes.
+   * @param {boolean} verbose - Whether to print verbose logs or not. Defaults to false.
+   */
   getUser(params, headers, callback, verbose=false) {
     this.baseRequest.get({
       url:     '/users',
@@ -43,7 +68,16 @@ class ThunderClient {
     });
   }
 
-  /* PUT /users */
+  /**
+   * PUT /users
+   * Updates a Thunder user.
+   *
+   * @param {object} params - The query parameters to attach to the URL.
+   * @param {object} body - The request body to PUT.
+   * @param {object} headers - The request headers to add to the request.
+   * @param {function} callback - The function to call when the method completes.
+   * @param {boolean} verbose - Whether to print verbose logs or not. Defaults to false.
+   */
   updateUser(params, body, headers, callback, verbose=false) {
     this.baseRequest.put({
       url:     '/users',
@@ -58,7 +92,15 @@ class ThunderClient {
     });
   }
 
-  /* DELETE /users */
+  /**
+   * DELETE /users
+   * Deletes a Thunder user.
+   *
+   * @param {object} params - The query parameters to attach to the URL.
+   * @param {object} headers - The request headers to add to the request.
+   * @param {function} callback - The function to call when the method completes.
+   * @param {boolean} verbose - Whether to print verbose logs or not. Defaults to false.
+   */
   deleteUser(params, headers, callback, verbose=false) {
     this.baseRequest.delete({
       url:     '/users',
@@ -71,7 +113,15 @@ class ThunderClient {
     });
   }
 
-  /* POST /verify */
+  /**
+   * POST /verify
+   * Creates a new verification email.
+   *
+   * @param {object} params - The query parameters to attach to the URL.
+   * @param {object} headers - The request headers to add to the request.
+   * @param {function} callback - The function to call when the method completes.
+   * @param {boolean} verbose - Whether to print verbose logs or not. Defaults to false.
+   */
   sendEmail(params, headers, callback, verbose=false) {
     this.baseRequest.post({
       url:     '/verify',
@@ -84,7 +134,15 @@ class ThunderClient {
     });
   }
 
-  /* GET /verify */
+  /**
+   * GET /verify
+   * Verifies a user. Simulates the user clicking the URL in the email.
+   *
+   * @param {object} params - The query parameters to attach to the URL.
+   * @param {object} headers - The request headers to add to the request.
+   * @param {function} callback - The function to call when the method completes.
+   * @param {boolean} verbose - Whether to print verbose logs or not. Defaults to false.
+   */
   verifyUser(params, headers, callback, verbose=false) {
     this.baseRequest.get({
       url:     '/verify',
@@ -98,6 +156,16 @@ class ThunderClient {
   }
 }
 
+/**
+ * Checks if a Thunder response was successful.
+ *
+ * @param {object} res - The response object to verify.
+ * @param {object} body - The reponse body.
+ * @param {Method} method - The method that was called to produce the response.
+ * @param {function} callback - The function to call when the method completes.
+ * @param {boolean} verbose - Whether to print verbose logs or not. Defaults to false.
+ * @return When the response check is complete.
+ */
 function checkResponse(res, body, method, callback, verbose=false) {
   if (res.statusCode !== method.expected) {
     console.log('An error occurred while performing method %s', method.name);
@@ -109,7 +177,8 @@ function checkResponse(res, body, method, callback, verbose=false) {
 
     console.log('\n');
     return callback(
-      new Error('The status code ' + res.statusCode + ' does not match expected ' + method.expected));
+      new Error('The status code ' + res.statusCode
+        + ' does not match expected ' + method.expected));
   }
 
   console.log('Successfully completed method %s', method.name);
