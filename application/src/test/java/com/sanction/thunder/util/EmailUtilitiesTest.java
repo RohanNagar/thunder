ffb7a740d74139691a60bfc886c046439d02ef42
Rohan Nagar <rohan.nagar@utexas.edu>
2018-04-27 18:23:39 -0700

    Add some test

---
+++ application/src/test/java/com/sanction/thunder/util/EmailUtilitiesTest.java
@@ -0,0 +1,28 @@
+package com.sanction.thunder.util;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class EmailUtilitiesTest {
+  private static final String URL_PLACEHOLDER = "CODEGEN-URL";
+  private static final String URL_ENDPOINT = "/verify?email=%s&token=%s&response_type=html";
+
+  @Test
+  public void testReplacePlaceholderNoUrl() {
+    String contents = "test contents";
+    String url = "http://www.test.com";
+
+    assertEquals(contents, EmailUtilities.replaceUrlPlaceholder(contents, url));
+  }
+
+  @Test
+  public void testReplacePlaceholderWithUrl() {
+    String contents = "test contents " + URL_PLACEHOLDER;
+    String url = "http://www.test.com";
+
+    String expected = "test contents " + url;
+
+    assertEquals(expected, EmailUtilities.replaceUrlPlaceholder(contents, url));
+  }
+}
