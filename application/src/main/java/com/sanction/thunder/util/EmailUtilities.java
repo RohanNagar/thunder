46c352262cf57c12b5c3eb352c629d78e0a8f4f9
Rohan Nagar <rohan.nagar@utexas.edu>
2018-04-30 20:20:18 -0700

    Warn if no instance of URL_PLACEHOLDER exists

--- application/src/main/java/com/sanction/thunder/util/EmailUtilities.java
+++ application/src/main/java/com/sanction/thunder/util/EmailUtilities.java
@@ -1,10 +1,26 @@
 package com.sanction.thunder.util;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public class EmailUtilities {
+  private static final Logger LOG = LoggerFactory.getLogger(EmailUtilities.class);
+
   private static final String URL_PLACEHOLDER = "CODEGEN-URL";
   private static final String URL_ENDPOINT = "verify?email=%s&token=%s&response_type=html";
 
+  /**
+   * Replaces the placeholder inside the given file contents with the given URL.
+   * @param fileContents The file contents to replace in.
+   * @param url The URL to insert in place of the placeholder.
+   * @return A string with modified file contents including the URL.
+   */
   public static String replaceUrlPlaceholder(String fileContents, String url) {
+    if (!fileContents.contains(URL_PLACEHOLDER)) {
+      LOG.warn("The email file contents do not contain any instances of the URL placeholder {}",
+          URL_PLACEHOLDER);
+    }
+
     return fileContents.replaceAll(URL_PLACEHOLDER, url);
   }
 
