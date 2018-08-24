0cbc0007846476e4540e97c425bb8f0f6114f1e7
Rohan Nagar <rohan.nagar@utexas.edu>
2018-08-24 21:12:15 +0000

    New integration tests

--- scripts/ci/docker_compose_test.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# Get the test file from passed arguments
-TEST_FILE=$1
-
-docker-compose -f scripts/tests/general/docker-compose.yml up -d
-sleep 10 # Wait for Thunder to start
-
-node scripts/tests/test-runner.js "$TEST_FILE"
