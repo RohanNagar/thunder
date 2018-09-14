b5179c6255406c427c15f6e978eedc2eafa4a149
Rohan Nagar <rohan.nagar@utexas.edu>
2018-09-13 20:21:36 -0700

    Implement MD5

--- application/src/main/java/com/sanctionco/thunder/crypto/MD5HashService.java
+++ application/src/main/java/com/sanctionco/thunder/crypto/MD5HashService.java
@@ -1,9 +1,19 @@
 package com.sanctionco.thunder.crypto;
 
+import java.nio.charset.StandardCharsets;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
+import org.apache.commons.codec.binary.Hex;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 /**
  * Provides hashing and verifying methods implemented with MD5.
  */
 public class MD5HashService implements HashService {
+  private static final Logger LOG = LoggerFactory.getLogger(MD5HashService.class);
 
   /**
    * Determines if the given string matches the given hashed string.
@@ -14,7 +24,19 @@ public class MD5HashService implements HashService {
    */
   @Override
   public boolean isMatch(String plaintext, String hashed) {
-    // TODO
-    return plaintext.equals(hashed);
+    String computedHash;
+
+    try {
+      MessageDigest md = MessageDigest.getInstance("MD5");
+
+      byte[] digest = md.digest(plaintext.getBytes(StandardCharsets.UTF_8));
+      computedHash = Hex.encodeHexString(digest);
+    } catch (NoSuchAlgorithmException e) {
+      LOG.error("An expected error occurred while computing an MD5 hash. "
+          + "Requests are going to fail while this continues.", e);
+      return false;
+    }
+    
+    return computedHash.equals(hashed);
   }
 }
--- application/src/test/java/com/sanctionco/thunder/crypto/HashAlgorithmTest.java
+++ application/src/test/java/com/sanctionco/thunder/crypto/HashAlgorithmTest.java
@@ -10,34 +10,38 @@ class HashAlgorithmTest {
 
   @Test
   void testBcryptResponseTypeMapping() {
-    assertAll("Assert equal JSON enum and string value",
+    assertAll("Assert equal BCRYPT enum and string value",
         () -> assertEquals(HashAlgorithm.BCRYPT, HashAlgorithm.fromString("bcrypt")),
         () -> assertEquals("bcrypt", HashAlgorithm.BCRYPT.toString()),
-        () -> assertEquals(BCryptHashService.class, HashAlgorithm.BCRYPT.newHashService().getClass()));
+        () -> assertEquals(BCryptHashService.class,
+            HashAlgorithm.BCRYPT.newHashService().getClass()));
   }
 
   @Test
   void testMd5ResponseTypeMapping() {
-    assertAll("Assert equal JSON enum and string value",
+    assertAll("Assert equal MD5 enum and string value",
         () -> assertEquals(HashAlgorithm.MD5, HashAlgorithm.fromString("md5")),
         () -> assertEquals("md5", HashAlgorithm.MD5.toString()),
-        () -> assertEquals(MD5HashService.class, HashAlgorithm.MD5.newHashService().getClass()));
+        () -> assertEquals(MD5HashService.class,
+            HashAlgorithm.MD5.newHashService().getClass()));
   }
 
   @Test
   void testSimpleResponseTypeMapping() {
-    assertAll("Assert equal JSON enum and string value",
+    assertAll("Assert equal SIMPLE enum and string value",
         () -> assertEquals(HashAlgorithm.SIMPLE, HashAlgorithm.fromString("simple")),
         () -> assertEquals("simple", HashAlgorithm.SIMPLE.toString()),
-        () -> assertEquals(SimpleHashService.class, HashAlgorithm.SIMPLE.newHashService().getClass()));
+        () -> assertEquals(SimpleHashService.class,
+            HashAlgorithm.SIMPLE.newHashService().getClass()));
   }
 
   @Test
   void testNoneResponseTypeMapping() {
-    assertAll("Assert equal JSON enum and string value",
+    assertAll("Assert equal NONE enum and string value",
         () -> assertEquals(HashAlgorithm.NONE, HashAlgorithm.fromString("none")),
         () -> assertEquals("none", HashAlgorithm.NONE.toString()),
-        () -> assertEquals(SimpleHashService.class, HashAlgorithm.NONE.newHashService().getClass()));
+        () -> assertEquals(SimpleHashService.class,
+            HashAlgorithm.NONE.newHashService().getClass()));
   }
 
   @Test
