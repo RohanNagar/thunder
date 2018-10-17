b3a6077cb8bcae44041fa495356d990eaa438a3b
Rohan Nagar <rohan.nagar@utexas.edu>
2018-10-17 08:20:01 -0700

    Add httpdomain extension

--- docs/conf.py
+++ docs/conf.py
@@ -39,6 +39,8 @@ release = ''
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
+  'sphinx.ext.autodoc',
+  'sphinxcontrib.httpdomain'
 ]
 
 # Add any paths that contain templates here, relative to this directory.
