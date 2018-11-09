9eed5fa2d1fa9b9de8849e466de30c3d6850c04a
Rohan Nagar <rohan.nagar@utexas.edu>
2018-11-09 08:43:38 -0800

    Fix javadoc comment

--- application/src/main/java/com/sanctionco/thunder/crypto/SimpleHashService.java
+++ application/src/main/java/com/sanctionco/thunder/crypto/SimpleHashService.java
@@ -1,7 +1,7 @@
 package com.sanctionco.thunder.crypto;
 
 /**
- * Provides the simple implementation for the {@link HashService}.  Provides methods to hash and to
+ * Provides a simple implementation for the {@link HashService}. Provides methods to hash and to
  * verify existing hashes match. This implementation can be used to compare without actually
  * performing a hash.
  *
