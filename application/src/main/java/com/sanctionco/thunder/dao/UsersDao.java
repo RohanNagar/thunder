f62702270241b4021411041e42a6eae8fb8282fd
Rohan Nagar <rohan.nagar@utexas.edu>
2019-09-26 10:00:06 -0700

    Add InvalidJavadocPosition check

--- application/src/main/java/com/sanctionco/thunder/dao/UsersDao.java
+++ application/src/main/java/com/sanctionco/thunder/dao/UsersDao.java
@@ -1,12 +1,10 @@
 package com.sanctionco.thunder.dao;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.JsonMappingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 import com.sanctionco.thunder.models.User;
 
-import java.io.IOException;
 import javax.annotation.Nullable;
 
 /**
