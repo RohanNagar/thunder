3baf422036f3af2196e27f38c6ea5524f48f325c
Rohan Nagar <rohan.nagar@utexas.edu>
2021-03-31 16:13:47 -0700

    Add allowSubset and allowSuperset options to property validation

---
+++ application/src/main/java/com/sanctionco/thunder/validation/PropertyValidationConfiguration.java
@@ -0,0 +1,47 @@
+package com.sanctionco.thunder.validation;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import java.util.Collections;
+import java.util.List;
+
+import javax.validation.Valid;
+
+/**
+ * Provides optional configuration options for property validation, including the validated
+ * properties. See the {@code ThunderConfiguration} class for more details.
+ */
+public class PropertyValidationConfiguration {
+  private static final boolean DEFAULT_ALLOW_SUPERSET = true;
+  private static final boolean DEFAULT_ALLOW_SUBSET = true;
+
+  /**
+   * Constructs a new instance of {@code PropertyValidationConfiguration} with default values.
+   */
+  public PropertyValidationConfiguration() {
+    this.allowSuperset = DEFAULT_ALLOW_SUPERSET;
+    this.allowSubset = DEFAULT_ALLOW_SUBSET;
+    this.validationRules = Collections.emptyList();
+  }
+
+  @Valid @JsonProperty("allowSuperset")
+  private final Boolean allowSuperset;
+
+  public Boolean allowSuperset() {
+    return allowSuperset;
+  }
+
+  @Valid @JsonProperty("allowSubset")
+  private final Boolean allowSubset;
+
+  public Boolean allowSubset() {
+    return allowSubset;
+  }
+
+  @Valid @JsonProperty("allowed")
+  private final List<PropertyValidationRule> validationRules;
+
+  public List<PropertyValidationRule> getValidationRules() {
+    return validationRules;
+  }
+}
--- application/src/main/java/com/sanctionco/thunder/validation/PropertyValidator.java
+++ application/src/main/java/com/sanctionco/thunder/validation/PropertyValidator.java
@@ -2,6 +2,8 @@ package com.sanctionco.thunder.validation;
 
 import java.util.List;
 import java.util.Map;
+import java.util.Objects;
+import java.util.stream.Collectors;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -12,17 +14,15 @@ import org.slf4j.LoggerFactory;
 public class PropertyValidator {
   private static final Logger LOG = LoggerFactory.getLogger(PropertyValidator.class);
 
-  private final List<PropertyValidationRule> validationRules;
-  private final boolean skipValidation;
+  private final PropertyValidationConfiguration validationOptions;
 
   /**
-   * Constructs a new {@code PropertyValidator} with the given validation rules.
+   * Constructs a new {@code PropertyValidator} with the given validation configuration.
    *
-   * @param validationRules the rules used to validate user property maps
+   * @param validationOptions the configuration used to validate user property maps
    */
-  public PropertyValidator(List<PropertyValidationRule> validationRules) {
-    this.skipValidation = validationRules == null;
-    this.validationRules = validationRules;
+  public PropertyValidator(PropertyValidationConfiguration validationOptions) {
+    this.validationOptions = Objects.requireNonNull(validationOptions);
   }
 
   /**
@@ -32,21 +32,69 @@ public class PropertyValidator {
    * @return {@code true} if the property map is valid; {@code false} otherwise
    */
   public boolean isValidPropertiesMap(Map<String, Object> properties) {
-    if (skipValidation) {
-      LOG.info("Skipping property validation because no properties were specified.");
-      return true;
+    // Both false. Users can not have extra fields or less than those specified.
+    // All specified fields must exist and be correct, and no more.
+    if (!validationOptions.allowSuperset() && !validationOptions.allowSubset()) {
+      if (properties.size() != validationOptions.getValidationRules().size()) {
+        LOG.info("Properties size does not match the number of validation rules.");
+        return false;
+      }
+
+      return validationOptions.getValidationRules().stream()
+          .allMatch(rule -> properties.containsKey(rule.getName())
+              && rule.getType().isInstance(properties.get(rule.getName())));
     }
 
-    // Check for size
-    if (properties.size() != validationRules.size()) {
-      LOG.info("Properties size does not match the number of validation rules.");
-      return false;
+    // allowSuperset true and allowSubset false. Users can have extra fields than those specified,
+    // but no less than those specified.
+    if (validationOptions.allowSuperset() && !validationOptions.allowSubset()) {
+      // Properties can be greater but not fewer
+      if (properties.size() < validationOptions.getValidationRules().size()) {
+        LOG.info("Properties size is less than the allowed rules and subset is not allowed.");
+        return false;
+      }
+
+      return validationOptions.getValidationRules().stream()
+          .allMatch(rule -> properties.containsKey(rule.getName())
+              && rule.getType().isInstance(properties.get(rule.getName())));
     }
 
-    // Match name and type
-    return validationRules.stream()
-        .allMatch(rule -> properties.containsKey(rule.getName())
-            && rule.getType().isInstance(properties.get(rule.getName())));
+    // allowSuperset false and allowSubset true. Users can not have extra fields,
+    // but they can have less. All properties must be in the list of specified properties.
+    if (!validationOptions.allowSuperset() && validationOptions.allowSubset()) {
+      // Properties can be fewer but not greater
+      if (properties.size() > validationOptions.getValidationRules().size()) {
+        LOG.info("Properties size is greater than the allowed rules and superset is not allowed.");
+        return false;
+      }
+
+      // Make sure all properties names exist in the rules
+      Map<String, Class<?>> allowedMap = validationOptions.getValidationRules().stream()
+          .collect(Collectors.toMap(
+              PropertyValidationRule::getName,
+              PropertyValidationRule::getType));
+
+      return properties.entrySet().stream().allMatch(entry ->
+          allowedMap.containsKey(entry.getKey())
+              && allowedMap.get(entry.getKey()).isInstance(entry.getValue()));
+    }
+
+    // Both true. Users can have extra fields than those specified, or less than those specified,
+    // but the ones that are presents and specified will be checked to make sure they are
+    // the correct type.
+    Map<String, Class<?>> allowedMap = validationOptions.getValidationRules().stream()
+        .collect(Collectors.toMap(
+            PropertyValidationRule::getName,
+            PropertyValidationRule::getType));
+
+    for (Map.Entry<String, Object> entry : properties.entrySet()) {
+      if (allowedMap.containsKey(entry.getKey())
+          && !allowedMap.get(entry.getKey()).isInstance(entry.getValue())) {
+        return false;
+      }
+    }
+
+    return true;
   }
 
   /**
@@ -68,9 +116,9 @@ public class PropertyValidator {
    */
   static Class<?> getType(String typename) {
     return switch (typename) {
-      case "string" -> String.class;
-      case "integer" -> Integer.class;
-      case "boolean" -> Boolean.class;
+      case "string", "str" -> String.class;
+      case "integer", "int" -> Integer.class;
+      case "boolean", "bool" -> Boolean.class;
       case "double" -> Double.class;
       case "list" -> List.class;
       case "map" -> Map.class;
