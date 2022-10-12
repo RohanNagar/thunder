3e60ea9288b46efb3ff1b6c923d4822013b186ff
Rohan Nagar <rohan.nagar@utexas.edu>
2022-10-12 16:57:12 -0700

    Parameterize common HashService unit tests

--- application/src/test/java/com/sanctionco/thunder/crypto/Sha256HashServiceTest.java
+++ application/src/test/java/com/sanctionco/thunder/crypto/Sha256HashServiceTest.java
@@ -2,7 +2,6 @@ package com.sanctionco.thunder.crypto;
 
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
@@ -31,110 +30,5 @@ class Sha256HashServiceTest {
 
     assertFalse(hashService.isMatch(plaintext, hashed));
     assertFalse(hashService.isMatch(plaintext, uppercase));
-  }
-
-  @Test
-  void testHashSame() {
-    String plaintext = "password";
-    String secondPlaintext = "password";
-
-    String result = hashService.hash(plaintext);
-    String secondResult = hashService.hash(secondPlaintext);
-
-    assertTrue(hashService.isMatch(plaintext, result));
-    assertTrue(hashService.isMatch(secondPlaintext, secondResult));
-
-    assertTrue(hashService.isMatch(plaintext, secondResult));
-    assertTrue(hashService.isMatch(secondPlaintext, result));
-  }
-
-  @Test
-  void testHashDifferent() {
-    String plaintext = "password";
-    String secondPlaintext = "secondPassword";
-
-    String result = hashService.hash(plaintext);
-    String secondResult = hashService.hash(secondPlaintext);
-
-    assertTrue(hashService.isMatch(plaintext, result));
-    assertTrue(hashService.isMatch(secondPlaintext, secondResult));
-
-    assertFalse(hashService.isMatch(plaintext, secondResult));
-    assertFalse(hashService.isMatch(secondPlaintext, result));
-  }
-
-  @Test
-  void testHashWithMistakesDisabled() {
-    String plaintext = "Password";
-    String hashed = hashService.hash(plaintext);
-
-    assertTrue(hashService.isMatch(plaintext, hashed));
-
-    String capsLock = "pASSWORD";
-    assertFalse(hashService.isMatch(capsLock, hashed));
-
-    String extraFirstCharacter = "*Password";
-    assertFalse(hashService.isMatch(extraFirstCharacter, hashed));
-
-    String extraLastCharacter = "Password-";
-    assertFalse(hashService.isMatch(extraLastCharacter, hashed));
-
-    String firstCharacterCaseSwap = "password";
-    assertFalse(hashService.isMatch(firstCharacterCaseSwap, hashed));
-  }
-
-  @Test
-  void testHashWithMistakesEnabled() {
-    HashService hashService = new Sha256HashService(true, true);
-
-    String plaintext = "Password";
-    String hashed = hashService.hash(plaintext);
-
-    assertTrue(hashService.isMatch(plaintext, hashed));
-
-    String capsLock = "pASSWORD";
-    assertTrue(hashService.isMatch(capsLock, hashed));
-
-    String extraFirstCharacter = "*Password";
-    assertTrue(hashService.isMatch(extraFirstCharacter, hashed));
-
-    String extraLastCharacter = "Password-";
-    assertTrue(hashService.isMatch(extraLastCharacter, hashed));
-
-    String firstCharacterCaseSwap = "password";
-    assertTrue(hashService.isMatch(firstCharacterCaseSwap, hashed));
-  }
-
-  @Test
-  void testHashWithMistakesEnabledIncorrect() {
-    HashService hashService = new Sha256HashService(true, true);
-
-    String plaintext = "Password";
-    String hashed = hashService.hash(plaintext);
-
-    assertTrue(hashService.isMatch(plaintext, hashed));
-
-    String incorrectCapsLock = "PASSWORD";
-    assertFalse(hashService.isMatch(incorrectCapsLock, hashed));
-
-    String incorrectExtraFirstCharacter = "*2Password";
-    assertFalse(hashService.isMatch(incorrectExtraFirstCharacter, hashed));
-
-    String incorrectExtraLastCharacter = "Password-x";
-    assertFalse(hashService.isMatch(incorrectExtraLastCharacter, hashed));
-
-    String incorrectFirstCharacterCaseSwap = "passworD";
-    assertFalse(hashService.isMatch(incorrectFirstCharacterCaseSwap, hashed));
-  }
-
-  @Test
-  void testServerSideHashDisabled() {
-    HashService hashService = new Sha256HashService(false, false);
-
-    String plaintext = "password";
-    String result = hashService.hash(plaintext);
-
-    // No hashing should be performed
-    assertEquals(plaintext, result);
   }
 }
