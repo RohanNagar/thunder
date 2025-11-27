297c9f6d0da2673fb44d4aa82ebffe170097195a
Rohan Nagar <rohannagar11@gmail.com>
2025-11-26 16:15:28 -0800

    revert change to test

--- scripts/tests/bad-request/test.js
+++ scripts/tests/bad-request/test.js
@@ -1,4 +1,4 @@
-import { describe, expect } from 'https://jslib.k6.io/k6chaijs/4.3.4.3/index.js';
+import { describe } from 'https://jslib.k6.io/expect/0.0.5/index.js';
 import { Httpx } from 'https://jslib.k6.io/httpx/0.1.0/index.js';
 
 export const options = {
@@ -7,7 +7,7 @@ export const options = {
       threshold:   'rate == 1.00',
       abortOnFail: true
     }],
-    http_req_duration: ['p(95)<100'] // 95% of requests should be below 100ms
+    http_req_duration: ['p(95)<100'] // 99% of requests should be below 100ms
   },
   // 10 virtual users running the test suite 10 times each
   vus:        10,
@@ -51,240 +51,262 @@ const invalidPropertiesUser = {
 };
 
 export default function testSuite() {
-  describe('create a null user', () => {
+  describe('create a null user', (t) => {
     const resp = session.post(`/users`, null,
         { headers: { 'Content-Type': 'application/json' } });
 
-    expect(resp.status).to.equal(400);
-    expect(resp.body).to.equal('Cannot post a null user. (User: null)');
-  });
+    t.expect(resp.status).as('status').toEqual(400)
+        .and(resp.body).as('error message')
+        .toEqual('Cannot post a null user. (User: null)');
+  }) &&
 
-  describe('create a user without an email address', () => {
+  describe('create a user without an email address', (t) => {
     const resp = session.post(`/users`, JSON.stringify({ email: null }),
         { headers: { 'Content-Type': 'application/json' } });
 
-    expect(resp.status).to.equal(400);
-    expect(resp.body).to.equal('Cannot post a user without an email address. (User: null)');
-  });
+    t.expect(resp.status).as('status').toEqual(400)
+        .and(resp.body).as('error message')
+        .toEqual('Cannot post a user without an email address. (User: null)');
+  }) &&
 
-  describe('create a user with an invalid email address', () => {
+  describe('create a user with an invalid email address', (t) => {
     const resp = session.post(`/users`, JSON.stringify({ email: { address: 'invalid' } }),
         { headers: { 'Content-Type': 'application/json' } });
 
-    expect(resp.status).to.equal(400);
-    expect(resp.body).to.equal('Invalid email address format. Please try again. (User: invalid)');
-  });
+    t.expect(resp.status).as('status').toEqual(400)
+        .and(resp.body).as('error message')
+        .toEqual('Invalid email address format. Please try again. (User: invalid)');
+  }) &&
 
-  describe('create a user with invalid properties', () => {
+  describe('create a user with invalid properties', (t) => {
     const resp = session.post(`/users`, JSON.stringify({ email: { address: 'test@test.com' }, password: 'test' }),
         { headers: { 'Content-Type': 'application/json' } });
 
-    expect(resp.status).to.equal(400);
-    expect(resp.body).to.equal('Cannot post a user with invalid properties. (User: test@test.com)');
-  });
+    t.expect(resp.status).as('status').toEqual(400)
+        .and(resp.body).as('error message')
+        .toEqual('Cannot post a user with invalid properties. (User: test@test.com)');
+  }) &&
 
-  describe('get a user without an email address', () => {
+  describe('get a user without an email address', (t) => {
     const resp = session.get(`/users`, null,
         { headers: { 'password': 'password' } });
 
-    expect(resp.status).to.equal(400);
-    expect(resp.body).to.equal('Incorrect or missing email query parameter. (User: null)');
-  });
+    t.expect(resp.status).as('status').toEqual(400)
+        .and(resp.body).as('error message')
+        .toEqual('Incorrect or missing email query parameter. (User: null)');
+  }) &&
 
-  describe('get a user with an empty email address', () => {
+  describe('get a user with an empty email address', (t) => {
     const resp = session.get(`/users?email=`, null,
         { headers: { 'password': 'password' } });
 
-    expect(resp.status).to.equal(400);
-    expect(resp.body).to.equal('Incorrect or missing email query parameter. (User: )');
-  });
+    t.expect(resp.status).as('status').toEqual(400)
+        .and(resp.body).as('error message')
+        .toEqual('Incorrect or missing email query parameter. (User: )');
+  }) &&
 
-  describe('get a user without a password header', () => {
+  describe('get a user without a password header', (t) => {
     const resp = session.get(`/users?email=success@simulator.amazonses.com`);
 
-    expect(resp.status).to.equal(400);
-    expect(resp.body)
-        .to.equal('Credentials are required to access this resource. (User: success@simulator.amazonses.com)');
-  });
+    t.expect(resp.status).as('status').toEqual(400)
+        .and(resp.body).as('error message')
+        .toEqual('Credentials are required to access this resource. (User: success@simulator.amazonses.com)');
+  }) &&
 
-  describe('get a user with an empty password header', () => {
+  describe('get a user with an empty password header', (t) => {
     const resp = session.get(`/users?email=success@simulator.amazonses.com`, null,
         { headers: { 'password': '' } });
 
-    expect(resp.status).to.equal(400);
-    expect(resp.body)
-        .to.equal('Credentials are required to access this resource. (User: success@simulator.amazonses.com)');
-  });
+    t.expect(resp.status).as('status').toEqual(400)
+        .and(resp.body).as('error message')
+        .toEqual('Credentials are required to access this resource. (User: success@simulator.amazonses.com)');
+  }) &&
 
-  describe('send a verification email to a null email address', () => {
+  describe('send a verification email to a null email address', (t) => {
     const resp = session.post(`/verify`, null,
         { headers: { 'password': 'password' } });
 
-    expect(resp.status).to.equal(400);
-    expect(resp.body).to.equal('Incorrect or missing email query parameter. (User: null)');
-  });
+    t.expect(resp.status).as('status').toEqual(400)
+        .and(resp.body).as('error message')
+        .toEqual('Incorrect or missing email query parameter. (User: null)');
+  }) &&
 
-  describe('send a verification email to an empty email address', () => {
+  describe('send a verification email to an empty email address', (t) => {
     const resp = session.post(`/verify?email=`, null,
         { headers: { 'password': 'password' } });
 
-    expect(resp.status).to.equal(400);
-    expect(resp.body).to.equal('Incorrect or missing email query parameter. (User: )');
-  });
+    t.expect(resp.status).as('status').toEqual(400)
+        .and(resp.body).as('error message')
+        .toEqual('Incorrect or missing email query parameter. (User: )');
+  }) &&
 
-  describe('send a verification email without a password header', () => {
+  describe('send a verification email without a password header', (t) => {
     const resp = session.post(`/verify?email=success@simulator.amazonses.com`);
 
-    expect(resp.status).to.equal(400);
-    expect(resp.body)
-        .to.equal('Credentials are required to access this resource. (User: success@simulator.amazonses.com)');
-  });
+    t.expect(resp.status).as('status').toEqual(400)
+        .and(resp.body).as('error message')
+        .toEqual('Credentials are required to access this resource. (User: success@simulator.amazonses.com)');
+  }) &&
 
-  describe('send a verification email with an empty password header', () => {
+  describe('send a verification email with an empty password header', (t) => {
     const resp = session.post(`/verify?email=success@simulator.amazonses.com`, null,
         { headers: { 'password': '' } });
 
-    expect(resp.status).to.equal(400);
-    expect(resp.body)
-        .to.equal('Credentials are required to access this resource. (User: success@simulator.amazonses.com)');
-  });
+    t.expect(resp.status).as('status').toEqual(400)
+        .and(resp.body).as('error message')
+        .toEqual('Credentials are required to access this resource. (User: success@simulator.amazonses.com)');
+  }) &&
 
-  describe('verify a null email address', () => {
+  describe('verify a null email address', (t) => {
     const resp = session.get(`/verify?token=testToken`);
 
-    expect(resp.status).to.equal(400);
-    expect(resp.body).to.equal('Incorrect or missing email query parameter. (User: null)');
-  });
+    t.expect(resp.status).as('status').toEqual(400)
+        .and(resp.body).as('error message')
+        .toEqual('Incorrect or missing email query parameter. (User: null)');
+  }) &&
 
-  describe('verify an empty email address', () => {
+  describe('verify an empty email address', (t) => {
     const resp = session.get(`/verify?email=&token=testToken`);
 
-    expect(resp.status).to.equal(400);
-    expect(resp.body).to.equal('Incorrect or missing email query parameter. (User: )');
-  });
+    t.expect(resp.status).as('status').toEqual(400)
+        .and(resp.body).as('error message')
+        .toEqual('Incorrect or missing email query parameter. (User: )');
+  }) &&
 
-  describe('verify with a null token', () => {
+  describe('verify with a null token', (t) => {
     const resp = session.get(`/verify?email=success@simulator.amazonses.com`);
 
-    expect(resp.status).to.equal(400);
-    expect(resp.body)
-        .to.equal('Incorrect or missing verification token query parameter. (User: success@simulator.amazonses.com)');
-  });
+    t.expect(resp.status).as('status').toEqual(400)
+        .and(resp.body).as('error message')
+        .toEqual('Incorrect or missing verification token query parameter. (User: success@simulator.amazonses.com)');
+  }) &&
 
-  describe('verify with an empty token', () => {
+  describe('verify with an empty token', (t) => {
     const resp = session.get(`/verify?email=success@simulator.amazonses.com&token=`);
 
-    expect(resp.status).to.equal(400);
-    expect(resp.body)
-        .to.equal('Incorrect or missing verification token query parameter. (User: success@simulator.amazonses.com)');
-  });
+    t.expect(resp.status).as('status').toEqual(400)
+        .and(resp.body).as('error message')
+        .toEqual('Incorrect or missing verification token query parameter. (User: success@simulator.amazonses.com)');
+  }) &&
 
-  describe('reset verification with a null email', () => {
+  describe('reset verification with a null email', (t) => {
     const resp = session.post(`/verify/reset`, null,
         { headers: { 'password': 'password' } });
 
-    expect(resp.status).to.equal(400);
-    expect(resp.body).to.equal('Incorrect or missing email query parameter. (User: null)');
-  });
+    t.expect(resp.status).as('status').toEqual(400)
+        .and(resp.body).as('error message')
+        .toEqual('Incorrect or missing email query parameter. (User: null)');
+  }) &&
 
-  describe('reset verification with an empty email', () => {
+  describe('reset verification with an empty email', (t) => {
     const resp = session.post(`/verify/reset?email=`, null,
         { headers: { 'password': 'password' } });
 
-    expect(resp.status).to.equal(400);
-    expect(resp.body).to.equal('Incorrect or missing email query parameter. (User: )');
-  });
+    t.expect(resp.status).as('status').toEqual(400)
+        .and(resp.body).as('error message')
+        .toEqual('Incorrect or missing email query parameter. (User: )');
+  }) &&
 
-  describe('reset verification without the password header', () => {
+  describe('reset verification without the password header', (t) => {
     const resp = session.post(`/verify/reset?email=success@simulator.amazonses.com`);
 
-    expect(resp.status).to.equal(400);
-    expect(resp.body)
-        .to.equal('Credentials are required to access this resource. (User: success@simulator.amazonses.com)');
-  });
+    t.expect(resp.status).as('status').toEqual(400)
+        .and(resp.body).as('error message')
+        .toEqual('Credentials are required to access this resource. (User: success@simulator.amazonses.com)');
+  }) &&
 
-  describe('reset verification with an empty password header', () => {
+  describe('reset verification with an empty password header', (t) => {
     const resp = session.post(`/verify/reset?email=success@simulator.amazonses.com`, null,
         { headers: { 'password': '' } });
 
-    expect(resp.status).to.equal(400);
-    expect(resp.body)
-        .to.equal('Credentials are required to access this resource. (User: success@simulator.amazonses.com)');
-  });
+    t.expect(resp.status).as('status').toEqual(400)
+        .and(resp.body).as('error message')
+        .toEqual('Credentials are required to access this resource. (User: success@simulator.amazonses.com)');
+  }) &&
 
-  describe('update a user without the password header', () => {
+  describe('update a user without the password header', (t) => {
     const resp = session.put(`/users`, JSON.stringify(validUserForUpdate),
         { headers: { 'Content-Type': 'application/json' } });
 
-    expect(resp.status).to.equal(400);
-    expect(resp.body).to.equal('Credentials are required to access this resource. (User: test@test.com)');
-  });
+    t.expect(resp.status).as('status').toEqual(400)
+        .and(resp.body).as('error message')
+        .toEqual('Credentials are required to access this resource. (User: test@test.com)');
+  }) &&
 
-  describe('update a user with an empty password header', () => {
+  describe('update a user with an empty password header', (t) => {
     const resp = session.put(`/users`, JSON.stringify(validUserForUpdate),
         { headers: { 'Content-Type': 'application/json', 'password': '' } });
 
-    expect(resp.status).to.equal(400);
-    expect(resp.body).to.equal('Credentials are required to access this resource. (User: test@test.com)');
-  });
+    t.expect(resp.status).as('status').toEqual(400)
+        .and(resp.body).as('error message')
+        .toEqual('Credentials are required to access this resource. (User: test@test.com)');
+  }) &&
 
-  describe('update with a null user object', () => {
+  describe('update with a null user object', (t) => {
     const resp = session.put(`/users`, null,
         { headers: { 'Content-Type': 'application/json', 'password': 'password' } });
 
-    expect(resp.status).to.equal(400);
-    expect(resp.body).to.equal('Cannot post a null user. (User: null)');
-  });
+    t.expect(resp.status).as('status').toEqual(400)
+        .and(resp.body).as('error message')
+        .toEqual('Cannot post a null user. (User: null)');
+  }) &&
 
-  describe('update with a user object that does not have an email address', () => {
+  describe('update with a user object that does not have an email address', (t) => {
     const resp = session.put(`/users`, JSON.stringify(missingEmailUser),
         { headers: { 'Content-Type': 'application/json', 'password': 'password' } });
 
-    expect(resp.status).to.equal(400);
-    expect(resp.body).to.equal('Cannot post a user without an email address. (User: null)');
-  });
+    t.expect(resp.status).as('status').toEqual(400)
+        .and(resp.body).as('error message')
+        .toEqual('Cannot post a user without an email address. (User: null)');
+  }) &&
 
-  describe('update with a user object that has an invalid email address', () => {
+  describe('update with a user object that has an invalid email address', (t) => {
     const resp = session.put(`/users`, JSON.stringify(invalidEmailUser),
         { headers: { 'Content-Type': 'application/json', 'password': 'password' } });
 
-    expect(resp.status).to.equal(400);
-    expect(resp.body).to.equal('Invalid email address format. Please try again. (User: invalid)');
-  });
+    t.expect(resp.status).as('status').toEqual(400)
+        .and(resp.body).as('error message')
+        .toEqual('Invalid email address format. Please try again. (User: invalid)');
+  }) &&
 
-  describe('update with a user object that has invalid properties', () => {
+  describe('update with a user object that has invalid properties', (t) => {
     const resp = session.put(`/users`, JSON.stringify(invalidPropertiesUser),
         { headers: { 'Content-Type': 'application/json', 'password': 'password' } });
 
-    expect(resp.status).to.equal(400);
-    expect(resp.body).to.equal('Cannot post a user with invalid properties. (User: test@test.com)');
-  });
+    t.expect(resp.status).as('status').toEqual(400)
+        .and(resp.body).as('error message')
+        .toEqual('Cannot post a user with invalid properties. (User: test@test.com)');
+  }) &&
 
-  describe('delete a null email address', () => {
+  describe('delete a null email address', (t) => {
     const resp = session.delete(`/users`, null, { headers: { 'password': 'password' } });
 
-    expect(resp.status).to.equal(400);
-    expect(resp.body).to.equal('Incorrect or missing email query parameter. (User: null)');
-  });
+    t.expect(resp.status).as('status').toEqual(400)
+        .and(resp.body).as('error message')
+        .toEqual('Incorrect or missing email query parameter. (User: null)');
+  }) &&
 
-  describe('delete an empty email address', () => {
+  describe('delete an empty email address', (t) => {
     const resp = session.delete(`/users?email=`, null, { headers: { 'password': 'password' } });
 
-    expect(resp.status).to.equal(400);
-    expect(resp.body).to.equal('Incorrect or missing email query parameter. (User: )');
-  });
+    t.expect(resp.status).as('status').toEqual(400)
+        .and(resp.body).as('error message')
+        .toEqual('Incorrect or missing email query parameter. (User: )');
+  }) &&
 
-  describe('delete a user without a password header', () => {
+  describe('delete a user without a password header', (t) => {
     const resp = session.delete(`/users?email=test@test.com`);
 
-    expect(resp.status).to.equal(400);
-    expect(resp.body).to.equal('Credentials are required to access this resource. (User: test@test.com)');
-  });
+    t.expect(resp.status).as('status').toEqual(400)
+        .and(resp.body).as('error message')
+        .toEqual('Credentials are required to access this resource. (User: test@test.com)');
+  }) &&
 
-  describe('delete a user with an empty password header', () => {
+  describe('delete a user with an empty password header', (t) => {
     const resp = session.delete(`/users?email=test@test.com`, null, { headers: { 'password': '' } });
 
-    expect(resp.status).to.equal(400);
-    expect(resp.body).to.equal('Credentials are required to access this resource. (User: test@test.com)');
+    t.expect(resp.status).as('status').toEqual(400)
+        .and(resp.body).as('error message')
+        .toEqual('Credentials are required to access this resource. (User: test@test.com)');
   });
 }
